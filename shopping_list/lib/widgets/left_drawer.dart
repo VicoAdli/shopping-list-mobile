e91136e8dfb2f012d209f2a24d817c5df6c13c16
VicoAdli <vicoadli@gmail.com>
2023-11-15 17:30:09 +0700

    Progress Tutorial 8

--- shopping_list/lib/widgets/left_drawer.dart
+++ shopping_list/lib/widgets/left_drawer.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+
+import 'package:shopping_list/screens/list_product.dart';
 import 'package:shopping_list/screens/menu.dart';
 import 'package:shopping_list/screens/shoplist_form.dart';
 
@@ -59,6 +61,18 @@ class LeftDrawer extends StatelessWidget {
                   context,
                   MaterialPageRoute(
                     builder: (context) => const ShopFormPage(),
+                  ));
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.shopping_basket),
+            title: const Text('Daftar Produk'),
+            // Bagian redirection ke ShopFormPage
+            onTap: () {
+              Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const ProductPage(),
                   ));
             },
           ),
--- shopping_list/lib/widgets/shop_card.dart
+++ shopping_list/lib/widgets/shop_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'package:shopping_list/screens/shoplist_form.dart';
 
 class ShopItem {
